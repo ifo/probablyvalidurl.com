8ad3bd2198b9c4c351d09421f7f3fa573c23bfa4
Steve McCarthy <steve@redlua.com>
2015-05-14 15:32:25 -0400

    Add shortenResponse and indexHandler tests
    With help from @icco

--- main_test.go
+++ main_test.go
@@ -1,9 +1,40 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestMakeKey(t *testing.T) {
 	if len(makeKey()) != 10 {
 		t.Errorf("makeKey() didn't return a string of length 10")
 	}
+}
+
+func TestShortenResponse(t *testing.T) {
+	request, err := http.NewRequest("GET", "http://example.com/", nil)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	shortenResponse(recorder, request, "http://example.com/")
+
+	fmt.Printf("%d - %s", recorder.Code, recorder.Body.String())
+}
+
+func TestIndexHandler(t *testing.T) {
+	request, err := http.NewRequest("GET", "http://example.com/", nil)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	indexHandler(recorder, request)
+
+	fmt.Printf("%d - %s", recorder.Code, recorder.Body.String())
 }
