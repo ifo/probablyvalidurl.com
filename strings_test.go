34eaf91cfa09125ad12b37879ca4d982608a294f
Steve McCarthy <steve@redlua.com>
2015-05-14 15:06:59 -0400

    Add invalid character tests for randomString
    With help from @icco

--- strings_test.go
+++ strings_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRandomString(t *testing.T) {
 	// random string of length 10
@@ -15,5 +18,24 @@ func TestRandomString(t *testing.T) {
 	check = randomString(length)
 	if len(check) != length {
 		t.Errorf("RandomString(%q) wasn't of the right length", length)
+	}
+}
+
+func TestRandomStringInvalidChars(t *testing.T) {
+	const invalidChars = "-_"
+	randString := randomString(256)
+	for _, v := range invalidChars {
+		if strings.Contains(randString, string(v)) {
+			t.Errorf("RandomString(256) contains invalid character %q", v)
+		}
+	}
+}
+
+func TestRandomStringValidChars(t *testing.T) {
+	randString := randomString(256)
+	for _, v := range randString {
+		if !strings.Contains(alphabet, string(v)) {
+			t.Errorf("RandomString(256) contains invalid character %q", v)
+		}
 	}
 }
